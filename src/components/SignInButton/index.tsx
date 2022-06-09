ecca141bf5b7e66e9f8c429cea27fe163a127125
Vitor Duarte <vitorduarte.vds7@gmail.com>
2022-06-09 16:02:43 -0300

    feat: added database

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -7,30 +7,24 @@ import styles from "./styles.module.scss";
 export function SignInButton() {
     const { data: session } = useSession();
 
-    console.log(session);
-
-    try {
-        return session ? (
-            <button
-                onClick={() => signOut()}
-                type="button"
-                className={styles.signInButton}
-            >
-                <FaGithub color="#04d361" />
-                {session.user.name}
-                <FiX color="#73780" className={styles.closeIcon} />
-            </button>
-        ) : (
-            <button
-                onClick={() => signIn("github")}
-                type="button"
-                className={styles.signInButton}
-            >
-                <FaGithub color="#eba417" />
-                Sign in with Github
-            </button>
-        );
-    } catch {
-        console.log(Error);
-    }
+    return session ? (
+        <button
+            onClick={() => signOut()}
+            type="button"
+            className={styles.signInButton}
+        >
+            <FaGithub color="#04d361" />
+            {session.user.name}
+            <FiX color="#73780" className={styles.closeIcon} />
+        </button>
+    ) : (
+        <button
+            onClick={() => signIn("github")}
+            type="button"
+            className={styles.signInButton}
+        >
+            <FaGithub color="#eba417" />
+            Sign in with Github
+        </button>
+    );
 }
